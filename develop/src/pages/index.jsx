e98bb752bea20899957167784575ac1f8179bd2d
zdxxxxxx <363635322@qq.com>
2017-04-27 21:15:37 +0800

    update

--- develop/src/pages/index.jsx
+++ develop/src/pages/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Input } from 'antd'
-
+import Nav from '../components/Nav.jsx'
 class Index extends Component {
     render() {
         return (
             <div>
-                <div>welcome! fard</div>
+                <Nav />
+                <div style={{ marginTop: 30 }}>welcome! fard</div>
                 <Input />
             </div>
         )
